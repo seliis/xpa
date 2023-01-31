122cd190ea62a4d555329b37a47761183c132f03
In Sun <ilovemyroro624@gmail.com>
2023-01-31 18:13:10 +0900

    CONTINUE

--- lib/view/mission/mission.dart
+++ lib/view/mission/mission.dart
@@ -1,13 +1,65 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:flutter/material.dart";
 
+class MissionCard extends ConsumerWidget {
+  const MissionCard({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 24,
+        ),
+        alignment: Alignment.centerLeft,
+        shape: const RoundedRectangleBorder(),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "missionName",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 20,
+            ),
+          ),
+          Text("assignDate"),
+        ],
+      ),
+    );
+  }
+}
+
 class Mission extends ConsumerWidget {
   const Mission({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Center(
-      child: Text("Mission"),
+    return Row(
+      children: [
+        Container(
+          //color: Colors.pink,
+          width: MediaQuery.of(context).size.width * 0.2,
+          child: ListView.separated(
+            itemCount: 3,
+            padding: const EdgeInsets.all(8),
+            itemBuilder: (BuildContext context, int index) {
+              return MissionCard();
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(
+                height: 8,
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: Container(),
+        ),
+      ],
     );
   }
 }
