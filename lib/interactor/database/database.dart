378d403d179cdb8aa6e8a4777d791b5f29691a41
In Sun <ilovemyroro624@gmail.com>
2023-02-11 02:30:03 +0900

    CONTINUE

--- lib/interactor/database/database.dart
+++ lib/interactor/database/database.dart
@@ -5,9 +5,25 @@ class Database {
   const Database._singleInstance();
   factory Database() => _instance;
 
-  static final Box testBox = Hive.box("test");
+  static final Box<dynamic>? _testBox;
 
-  static void saveData(Box box, dynamic key, dynamic value) {
+  static Future<Box<dynamic>> get testBox async {
+    return _testBox ?? await Hive.openBox("test");
+  }
+
+  static void write(Box box, dynamic key, dynamic value) {
     box.put(key, value);
+  }
+
+  static dynamic read(Box box, dynamic key) {
+    return box.get(key);
+  }
+
+  static void delete(Box box, dynamic key) {
+    box.delete(key);
+  }
+
+  static void clear(Box box, dynamic key) {
+    box.clear();
   }
 }
